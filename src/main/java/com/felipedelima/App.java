47a783cd6f0e1407c3ed16cfa2b6534e22c476b8
Felipe Lima <fe.max@hotmail.com>
2025-06-16 18:56:34 -0300

    first commit

---
+++ src/main/java/com/felipedelima/App.java
@@ -0,0 +1,13 @@
+package com.felipedelima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
